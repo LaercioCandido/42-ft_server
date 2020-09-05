bfbe6e5011591ae7c39848dc08f7704e571c441c
Laercio Candido <lcs_junior@yahoo.com.br>
2020-09-05 19:07:45 +0200

    organizando arquivos

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,21 @@
 # Baixa a imagem debian buster do Dockerhub
 FROM debian:buster
 
+# Copia a pasta srcs do host para a pasta root do container
+COPY /srcs /root
+
 # Instala e atualiza os pacotes apt, nginx, mariadb, php
 RUN apt-get update && \
     apt-get -y upgrade && \
-    apt-get install -y nginx \
+    apt-get install -y wget \
+    nginx \
     mariadb-server \
     php7.3 \
     php-fpm \
     php-mysql \
     php-cli \
-    php-mbstring \
-    wget
-
-# Copia a pasta srcs do host para a pasta root do container
-COPY srcs /root
-
-# Executa o server-config.sh, q faz configurações gerais
-RUN bash /root/server-config.sh
+    php-mbstring && \
+    bash /root/server-config.sh
 
 # A partir do bash executa o start.sh (que inicia o nginx, mysql)
 ENTRYPOINT bash /root/start.sh
--- srcs/server-config.sh
+++ srcs/server-config.sh
@@ -1,19 +1,17 @@
-# Configurações
-
+# Cria o diretório onde os arquivos serão salvos
 mkdir /var/www/localhost
 
-cp /root/nginx.conf /etc/nginx/sites-available/default
+# Entra no diretório raiz
+cd /root
 
-cp /root/info.php /var/www/localhost/
+# Movendo o arquivo que mostra informações do php para o localhost
+mv /root/info.php /var/www/localhost/
 
 # Instala o phpMyAdmin
-
 wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.tar.gz
-
 tar -xzvf phpMyAdmin-5.0.2-english.tar.gz
-
 rm -rf  phpMyAdmin-5.0.2-english.tar.gz
-
 mv  phpMyAdmin-5.0.2-english /var/www/localhost/phpmyadmin
-
+cp /root/nginx.conf /etc/nginx/sites-available/default
+cp /root/config.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 chown -R www-data:www-data /var/www/localhost/phpmyadmin
