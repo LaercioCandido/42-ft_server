b759c2073a7ac7492c1727bedfbb2c565f2b19fe
Laercio Candido <lcs_junior@yahoo.com.br>
2020-09-12 20:00:34 +0200

    add arquivos .tar e editando server-config.sh

--- srcs/server-config.sh
+++ srcs/server-config.sh
@@ -8,17 +8,33 @@ cd /root
 mv /root/info.php /var/www/localhost/
 
 # Instala o phpMyAdmin
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.tar.gz
-tar -xzvf phpMyAdmin-5.0.2-english.tar.gz
-rm -rf  phpMyAdmin-5.0.2-english.tar.gz
-mv  phpMyAdmin-5.0.2-english /var/www/localhost/phpmyadmin
+# wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xzvf phpMyAdmin-5.0.2-all-languages.tar.gz
+rm -rf  phpMyAdmin-5.0.2-all-languages.tar.gz
+mv  phpMyAdmin-5.0.2-all-languages /var/www/localhost/phpmyadmin
 cp /root/nginx.conf /etc/nginx/sites-available/default
 cp /root/config.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 chown -R www-data:www-data /var/www/localhost/phpmyadmin
 
+# Database e User do wordpress
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '1234';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+# Configurando superuser phpmyadmin
+service mysql start
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE USER 'lcandido'@'localhost' IDENTIFIED BY '1234';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'lcandido'@'localhost' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
 # Instala o wordpress
-wget https://br.wordpress.org/latest-pt_BR.tar.gz
-tar -xzvf latest-pt_BR.tar.gz
-rm -rf latest-pt_BR.tar.gz
-cp wp-config.php /wordpress/wp-config.php
-mv wordpress /var/www/localhost
+# wget https://br.wordpress.org/latest-pt_BR.tar.gz
+tar -xzvf wordpress-5.5.1-pt_BR.tar.gz
+rm -rf wordpress-5.5.1-pt_BR.tar.gz
+# cp wp-config.php /wordpress/wp-config.php
+mv wordpress /var/www/localhost/wordpress
+cp wp-config.php /var/www/localhost/wordpress
+
+
