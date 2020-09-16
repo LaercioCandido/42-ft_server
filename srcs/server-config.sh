5a2cd04e8dfc198489a2517701fe88b0b7244646
Laercio Candido <lcs_junior@yahoo.com.br>
2020-09-16 06:38:02 +0200

    yay \o/

--- srcs/server-config.sh
+++ srcs/server-config.sh
@@ -4,11 +4,7 @@ mkdir /var/www/localhost
 # Entra no diretório raiz
 cd /root
 
-# Movendo o arquivo que mostra informações do php para o localhost
-mv /root/info.php /var/www/localhost/
-
 # Instala o phpMyAdmin
-# wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar -xzvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm -rf  phpMyAdmin-5.0.2-all-languages.tar.gz
 mv  phpMyAdmin-5.0.2-all-languages /var/www/localhost/phpmyadmin
@@ -30,7 +26,6 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT 
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 # Instala o wordpress
-# wget https://br.wordpress.org/latest-pt_BR.tar.gz
 tar -xzvf wordpress-5.5.1-pt_BR.tar.gz
 rm -rf wordpress-5.5.1-pt_BR.tar.gz
 # cp wp-config.php /wordpress/wp-config.php
